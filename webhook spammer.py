43f1447d61690100a0fcb46e016fa9ec434dd2a1
Maximesama <103829091+Maximesama@users.noreply.github.com>
2022-04-16 13:48:38 +0200

    Add files via upload

---
+++ webhook spammer.py
@@ -0,0 +1,30 @@
+import requests
+import colorama
+from colorama import Fore
+colorama.init()
+
+banner = """
+    ___        ___        ___        ___     __  ___     __        ___
+  ((   ) )   //   ) )   //___) )   //   ) )   / /      //  ) )   //___) )
+   \ \      //___/ /   //         //         / /      //        //
+//   ) )   //         ((____     ((____     / /      //        ((____
+"""
+print(Fore.RED + banner)
+
+input()
+
+print(Fore.RED + 'Quel est votre Webhook? ')
+webhook = input("")
+print("Quel est votre message? ")
+mess = input("")
+content = (mess)
+
+payload = {
+    'content': (content)
+}
+def run_loop():
+    stop = False
+    while not stop:
+
+        r = requests.post(webhook, data=payload,)
+run_loop()
